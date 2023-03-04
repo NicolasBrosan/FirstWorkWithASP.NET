940214ed18fd23b953d735d88c042377681d3508
Nicolas Brosan <nicolasbrosan@hotmail.com>
2023-03-04 18:30:16 -0300

    Add project files.

---
+++ Visitas/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Visitas.Models;
+
+namespace Visitas.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Formulario()
+        {
+            return View();
+        }
+        public ActionResult CargaDeDatos()
+        {
+            var nombre = Request.Form["nombre"].ToString();
+            var comentarios = Request.Form["comentarios"].ToString();
+            var libro = new LibroDeVisitas();
+            libro.Grabar(nombre, comentarios);
+            return View();
+        }
+        public ActionResult ListadoDeVisitas()
+        {
+            var libro = new LibroDeVisitas();
+            string contenido = libro.Leer();
+            ViewData["libro"] = contenido;
+            return View();
+        }
+    }
+}
---
+++ Visitas/Models/LibroDeVisitas.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+
+namespace Visitas.Models
+{
+    public class LibroDeVisitas
+    {
+        public void Grabar(string nombre, string comentarios)
+        {
+            var archivo = new StreamWriter(HostingEnvironment.MapPath("~") + "/App_Data/datos.txt", true);
+            archivo.WriteLine("Nombre: " + nombre + "<br>Comentarios: " + comentarios + "<hr>");
+            archivo.Close();
+        }
+        public string Leer()
+        {
+            var archivo = new StreamReader(HostingEnvironment.MapPath("~") + "/App_Data/datos.txt");
+            string contenido = archivo.ReadToEnd();
+            archivo.Close();
+            return contenido;
+        }
+    }
+}
